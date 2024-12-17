6e7e0b9c97884a1c12eb37a2abff4df5c3243d0b
shashi yadav <shashiyadav1790@gmail.com>
2024-12-17 17:54:44 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./landingPage/home/Homepage.jsx";
-import Signup from "./landingPage/signup/Signup.jsx";
-import Login from "./landingPage/login/Login.jsx";
+
 import AboutPage from "./landingPage/about/AboutPage.jsx";
 import PricingPage from "./landingPage/pricing/PricingPage.jsx";
 import SupportPage from "./landingPage/support/Supportpage.jsx";
@@ -12,6 +11,8 @@ import "./index.css";
 import Navbar from "./landingPage/Navbar.jsx";
 import Footer from "./landingPage/Footer.jsx";
 import NotFound from "./landingPage/Notfound.jsx";
+import Login from "./landingPage/login/login.jsx";
+import SignUp from "./landingPage/signup/Signup.jsx";
 
 
 
@@ -29,7 +30,7 @@ createRoot(document.getElementById("root")).render(
   
       <Route path="/" element={<Homepage />}></Route>
     
-      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/about" element={<AboutPage />}></Route>
       <Route path="/pricing" element={<PricingPage/>}></Route>
